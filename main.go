53904d4f358ced7aac8d258e00f81a9fdff7f266
vinkel <vincent.benoot@gmail.com>
2024-08-20 11:11:10 +0200

    rotation

--- main.go
+++ main.go
@@ -12,9 +12,36 @@ var width = 1000
 var heigth = 1000
 var radian36 = math.Pi / 5
 
+var dart = polygon{
+	[]coordinate{
+		{0, 0},
+		{math.Cos(radian36), math.Sin(radian36)},
+		{1, 0},
+		{math.Cos(radian36), -math.Sin(radian36)},
+	},
+}
+
+var kite = polygon{
+	[]coordinate{
+		{0, 0},
+		{math.Cos(radian36), math.Sin(radian36)},
+		{1 / math.Phi, 0},
+		{math.Cos(radian36), -math.Sin(radian36)},
+	},
+}
+
 type coordinate struct {
 	x float64
 	y float64
+}
+
+type transformation struct {
+	rotation    float64
+	translation coordinate
+}
+
+type polygon struct {
+	points []coordinate
 }
 
 func main() {
@@ -26,27 +53,47 @@ func main() {
 func draw(writer http.ResponseWriter, _ *http.Request) {
 	canvas := svg.New(writer)
 	canvas.Start(width, heigth)
+	defer canvas.End()
 
-	canvas.Polygon(getXCoords(getKiteCoords()), getYCoords(getKiteCoords()), "fill:lightblue;stroke:black;stroke-width:2")
+	for i := 0; i < 5; i++ {
+		rotaionTransform := transformation{
+			2.0 * radian36 * float64(i),
+			coordinate{0, 0},
+		}
 
-	canvas.End()
-}
-
-func getDartCoords() []coordinate {
-	return []coordinate{
-		{0, 0},
-		{math.Cos(radian36), math.Sin(radian36)},
-		{1, 0},
-		{math.Cos(radian36), -math.Sin(radian36)},
+		drawPolygon(canvas, dart.applyTransformation(rotaionTransform))
 	}
 }
 
-func getKiteCoords() []coordinate {
-	return []coordinate{
-		{0, 0},
-		{math.Cos(radian36), math.Sin(radian36)},
-		{1 / math.Phi, 0},
-		{math.Cos(radian36), -math.Sin(radian36)},
+func drawPolygon(canvas *svg.SVG, shape polygon) {
+	canvas.Polygon(getXCoords(shape.points), getYCoords(shape.points), "fill:lightblue;stroke:black;stroke-width:2")
+}
+
+func (shape *polygon) applyTransformation(transform transformation) polygon {
+	newPoints := []coordinate{}
+	for _, coord := range shape.points {
+		newPoint := applyTransformation(coord, transform)
+		newPoints = append(newPoints, newPoint)
+	}
+	return polygon{newPoints}
+}
+
+func applyTransformation(coord coordinate, transform transformation) coordinate {
+	// rotation
+	angle := transform.rotation
+	rotationMatrix := [][]float64{
+		{math.Cos(angle), -math.Sin(angle)},
+		{math.Sin(angle), math.Cos(angle)},
+	}
+	// TODO: translation
+
+	return matrixTransform(coord, rotationMatrix)
+}
+
+func matrixTransform(coord coordinate, matrix [][]float64) coordinate {
+	return coordinate{
+		coord.x*matrix[0][0] + coord.y*matrix[0][1],
+		coord.x*matrix[1][0] + coord.y*matrix[1][1],
 	}
 }
 
