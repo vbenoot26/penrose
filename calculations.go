f506385be46f36e46dd2aa2c0de52421cf7e050c
vinkel <vincent.benoot@gmail.com>
2024-08-20 16:08:09 +0200

    wrote kitereplace with transforms instead of polygons

---
+++ calculations.go
@@ -0,0 +1,95 @@
+package main
+
+import "math"
+
+func drawPolygons() []polygon {
+	dartTransforms, kiteTransforms := kiteReplace()
+
+	result := []polygon{}
+	for _, trans := range dartTransforms {
+		result = append(result, dart.applyTransformation(trans))
+	}
+
+	for _, trans := range kiteTransforms {
+		result = append(result, kite.applyTransformation(trans))
+	}
+
+	return result
+}
+
+func (shape *polygon) applyTransformation(transform transformation) polygon {
+	newPoints := []coordinate{}
+	for _, coord := range shape.points {
+		newPoint := applyTransformation(coord, transform)
+		newPoints = append(newPoints, newPoint)
+	}
+	return polygon{newPoints}
+}
+
+func applyTransformation(coord coordinate, transform transformation) coordinate {
+	// rescales
+	rescale := math.Pow(scaleFactor, float64(transform.rescales))
+	newCoord := coordinate{
+		coord.x * rescale,
+		coord.y * rescale,
+	}
+
+	// rotation
+	angle := float64(transform.amountOfRotation) * radian36
+	rotationMatrix := [][]float64{
+		{math.Cos(angle), -math.Sin(angle)},
+		{math.Sin(angle), math.Cos(angle)},
+	}
+
+	newCoord = matrixTransform(newCoord, rotationMatrix)
+
+	// translation
+	newCoord = coordinate{
+		newCoord.x + transform.translation.x,
+		newCoord.y + transform.translation.y,
+	}
+
+	return newCoord
+}
+
+func matrixTransform(coord coordinate, matrix [][]float64) coordinate {
+	return coordinate{
+		coord.x*matrix[0][0] + coord.y*matrix[0][1],
+		coord.x*matrix[1][0] + coord.y*matrix[1][1],
+	}
+}
+
+func kiteReplace() ([]transformation, []transformation) {
+	return []transformation{
+			{0, coordinate{0, 0}, 1},
+		}, []transformation{
+			{6, coordinate{math.Cos(radian36), math.Sin(radian36)}, 1},
+			{-6, coordinate{math.Cos(radian36), -math.Sin(radian36)}, 1},
+		}
+}
+
+func dartReplace() []polygon {
+	dartAngle := 3 * radian36
+	return []polygon{
+		kite.applyTransformation(transformation{
+			1,
+			coordinate{0, 0},
+			1,
+		}),
+		kite.applyTransformation(transformation{
+			-1,
+			coordinate{0, 0},
+			1,
+		}),
+		dart.applyTransformation(transformation{
+			3,
+			coordinate{scaleFactor * (1 - math.Cos(dartAngle)), scaleFactor * -math.Sin(dartAngle)},
+			1,
+		}),
+		dart.applyTransformation(transformation{
+			-3,
+			coordinate{scaleFactor * (1 - math.Cos(-dartAngle)), scaleFactor * -math.Sin(-dartAngle)},
+			1,
+		}),
+	}
+}
--- main.go
+++ main.go
@@ -37,9 +37,9 @@ type coordinate struct {
 }
 
 type transformation struct {
-	rotation    float64
-	translation coordinate
-	rescales    int // rescales will always happen with the same factor: see scalefactor
+	amountOfRotation int // Since all rotation are multiples of radian36, we just save the factor in front of radian36
+	translation      coordinate
+	rescales         int // rescales will always happen with the same factor: see scalefactor
 }
 
 type polygon struct {
@@ -57,55 +57,13 @@ func draw(writer http.ResponseWriter, _ *http.Request) {
 	canvas.Start(width, heigth)
 	defer canvas.End()
 
-	for _, shape := range dartReplace() {
+	for _, shape := range drawPolygons() {
 		drawPolygon(canvas, shape)
 	}
 }
 
 func drawPolygon(canvas *svg.SVG, shape polygon) {
 	canvas.Polygon(getXCoords(shape.points), getYCoords(shape.points), "fill:lightblue;stroke:black;stroke-width:2")
-}
-
-func (shape *polygon) applyTransformation(transform transformation) polygon {
-	newPoints := []coordinate{}
-	for _, coord := range shape.points {
-		newPoint := applyTransformation(coord, transform)
-		newPoints = append(newPoints, newPoint)
-	}
-	return polygon{newPoints}
-}
-
-func applyTransformation(coord coordinate, transform transformation) coordinate {
-	// rescales
-	rescale := math.Pow(scaleFactor, float64(transform.rescales))
-	newCoord := coordinate{
-		coord.x * rescale,
-		coord.y * rescale,
-	}
-
-	// rotation
-	angle := transform.rotation
-	rotationMatrix := [][]float64{
-		{math.Cos(angle), -math.Sin(angle)},
-		{math.Sin(angle), math.Cos(angle)},
-	}
-
-	newCoord = matrixTransform(newCoord, rotationMatrix)
-
-	// translation
-	newCoord = coordinate{
-		newCoord.x + transform.translation.x,
-		newCoord.y + transform.translation.y,
-	}
-
-	return newCoord
-}
-
-func matrixTransform(coord coordinate, matrix [][]float64) coordinate {
-	return coordinate{
-		coord.x*matrix[0][0] + coord.y*matrix[0][1],
-		coord.x*matrix[1][0] + coord.y*matrix[1][1],
-	}
 }
 
 func getXCoords(coordinates []coordinate) []int {
@@ -124,48 +82,4 @@ func getYCoords(coordinates []coordinate) []int {
 		result = append(result, int((y*500)+500))
 	}
 	return result
-}
-
-func kiteReplace() []polygon {
-	return []polygon{
-		dart.applyTransformation(transformation{
-			0, coordinate{0, 0}, 1,
-		}),
-		kite.applyTransformation(transformation{
-			6 * math.Pi / 5,
-			coordinate{math.Cos(radian36), math.Sin(radian36)},
-			1,
-		}),
-		kite.applyTransformation(transformation{
-			-6 * math.Pi / 5,
-			coordinate{math.Cos(radian36), -math.Sin(radian36)},
-			1,
-		}),
-	}
-}
-
-func dartReplace() []polygon {
-	dartAngle := 3 * radian36
-	return []polygon{
-		kite.applyTransformation(transformation{
-			radian36,
-			coordinate{0, 0},
-			1,
-		}),
-		kite.applyTransformation(transformation{
-			-radian36,
-			coordinate{0, 0},
-			1,
-		}),
-		dart.applyTransformation(transformation{
-			dartAngle,
-			coordinate{scaleFactor * (1 - math.Cos(dartAngle)), scaleFactor * -math.Sin(dartAngle)},
-			1,
-		}),
-		dart.applyTransformation(transformation{
-			-dartAngle,
-			coordinate{scaleFactor * (1 - math.Cos(-dartAngle)), scaleFactor * -math.Sin(-dartAngle)},
-			1,
-		}),
-	}
 }
