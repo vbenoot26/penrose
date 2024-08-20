a627c466a1a687005b9a63f880e6f5aaecaeeb7b
vinkel <vincent.benoot@gmail.com>
2024-08-20 14:37:41 +0200

    non working kite replace

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 var width = 1000
 var heigth = 1000
 var radian36 = math.Pi / 5
+var scaleFactor = 2 - math.Phi
 
 var dart = polygon{
 	[]coordinate{
@@ -38,6 +39,7 @@ type coordinate struct {
 type transformation struct {
 	rotation    float64
 	translation coordinate
+	rescales    int // rescales will always happen with the same factor: see scalefactor
 }
 
 type polygon struct {
@@ -55,13 +57,8 @@ func draw(writer http.ResponseWriter, _ *http.Request) {
 	canvas.Start(width, heigth)
 	defer canvas.End()
 
-	for i := 0; i < 5; i++ {
-		rotaionTransform := transformation{
-			2.0 * radian36 * float64(i),
-			coordinate{0, 0},
-		}
-
-		drawPolygon(canvas, dart.applyTransformation(rotaionTransform))
+	for _, shape := range kiteReplace() {
+		drawPolygon(canvas, shape)
 	}
 }
 
@@ -79,15 +76,29 @@ func (shape *polygon) applyTransformation(transform transformation) polygon {
 }
 
 func applyTransformation(coord coordinate, transform transformation) coordinate {
+	// rescales
+	rescale := math.Pow(scaleFactor, float64(transform.rescales))
+	newCoord := coordinate{
+		coord.x * rescale,
+		coord.y * rescale,
+	}
+
 	// rotation
 	angle := transform.rotation
 	rotationMatrix := [][]float64{
 		{math.Cos(angle), -math.Sin(angle)},
 		{math.Sin(angle), math.Cos(angle)},
 	}
-	// TODO: translation
 
-	return matrixTransform(coord, rotationMatrix)
+	newCoord = matrixTransform(coord, rotationMatrix)
+
+	// translation
+	newCoord = coordinate{
+		newCoord.x + transform.translation.x,
+		newCoord.y + transform.translation.y,
+	}
+
+	return newCoord
 }
 
 func matrixTransform(coord coordinate, matrix [][]float64) coordinate {
@@ -113,4 +124,22 @@ func getYCoords(coordinates []coordinate) []int {
 		result = append(result, int((y*500)+500))
 	}
 	return result
+}
+
+func kiteReplace() []polygon {
+	return []polygon{
+		dart.applyTransformation(transformation{
+			0, coordinate{0, 0}, 1,
+		}),
+		kite.applyTransformation(transformation{
+			6 * math.Pi / 5,
+			coordinate{math.Cos(radian36), math.Sin(radian36)},
+			1,
+		}),
+		kite.applyTransformation(transformation{
+			-6 * math.Pi / 5,
+			coordinate{math.Cos(radian36), -math.Sin(radian36)},
+			1,
+		}),
+	}
 }
