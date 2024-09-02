19f86ff82e759993dee97aed129e91f739d78376
vinkel <vincent.benoot@gmail.com>
2024-09-02 12:17:38 +0200

    Drawing both tiles

--- game.go
+++ game.go
@@ -8,7 +8,27 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-var whiteImage = ebiten.NewImage(3, 3)
+var (
+	whiteImage = ebiten.NewImage(3, 3)
+	dartColor  = color.NRGBA{
+		R: 238,
+		G: 223,
+		B: 226,
+		A: 255,
+	}
+	// kiteColor = color.NRGBA{
+	// 	R: 159,
+	// 	G: 193,
+	// 	B: 49,
+	// 	A: 255,
+	// }
+	kiteColor = color.NRGBA{
+		R: 159,
+		G: 193,
+		B: 49,
+		A: 255,
+	}
+)
 
 const animationLength = 60
 
@@ -30,21 +50,21 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	for trans := range g.darts {
-		getDartCos(g.tick).applyTransformation(trans).draw(screen)
+		getDartCos(g.tick).applyTransformation(trans).draw(dartColor, screen)
 	}
 
 	for trans := range g.kites {
-		getKiteCos(g.tick).applyTransformation(trans).draw(screen)
+		getKiteCos(g.tick).applyTransformation(trans).draw(kiteColor, screen)
 	}
 }
 
 // We presume that the reciever is either a variant of a kite or a dart. This is important for the way the triangles are drawn.
-func (tile polygon) draw(screen *ebiten.Image) {
+func (tile polygon) draw(color color.NRGBA, screen *ebiten.Image) {
 	// coordinates to vertices
 	vertices := []ebiten.Vertex{}
 
 	for _, co := range tile.points {
-		vertices = append(vertices, newDartVertex(co))
+		vertices = append(vertices, co.toVertex(color))
 	}
 
 	// indices array, since this is the same for kites and darts we just magic value hard code it.
@@ -79,29 +99,16 @@ func getKiteCos(tick uint32) *polygon {
 	}
 }
 
-func newDartVertex(co coordinate) ebiten.Vertex {
+func (co *coordinate) toVertex(color color.NRGBA) ebiten.Vertex {
 	return ebiten.Vertex{
-		DstX:   getXCo(co),
-		DstY:   getYCo(co),
+		DstX:   getXCo(*co),
+		DstY:   getYCo(*co),
 		SrcX:   0,
 		SrcY:   0,
-		ColorR: 0.9333333333333333,
-		ColorG: 0.8745098039215686,
-		ColorB: 0.8862745098039215,
-		ColorA: 1,
-	}
-}
-
-func newKiteVertex(co coordinate) ebiten.Vertex {
-	return ebiten.Vertex{
-		DstX:   getXCo(co),
-		DstY:   getYCo(co),
-		SrcX:   0,
-		SrcY:   0,
-		ColorR: 0.6235294117647059,
-		ColorG: 0.7568627450980392,
-		ColorB: 0.19215686274509805,
-		ColorA: 1,
+		ColorR: float32(color.R) / 255,
+		ColorG: float32(color.G) / 255,
+		ColorB: float32(color.B) / 255,
+		ColorA: float32(color.A) / 255,
 	}
 }
 
--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main() {
 	ebiten.SetWindowTitle("Penrose")
 	// dartTranses, kiteTranses := calculateDrawing()
 	game := Game{
-		*createSet(idTransform), transSet{}, 0,
+		transSet{}, *createSet(idTransform), 0,
 	}
 	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
