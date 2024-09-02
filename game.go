488245a4619b69eff335d18c08dfc980d56e1adb
vinkel <vincent.benoot@gmail.com>
2024-09-02 10:39:11 +0200

    Made the angle of the drawing of a tile reliant on the tick (wip)

--- game.go
+++ game.go
@@ -3,11 +3,14 @@ package main
 import (
 	"image"
 	"image/color"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 var whiteImage = ebiten.NewImage(3, 3)
+
+const animationLength = 60
 
 func init() {
 	whiteImage.Fill(color.White)
@@ -17,46 +20,63 @@ func init() {
 type Game struct {
 	darts transSet
 	kites transSet
+	tick  uint32
 }
 
-func (g *Game) Update() error { return nil }
+func (g *Game) Update() error {
+	g.tick++
+	return nil
+}
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	for trans := range g.darts.items {
-		drawDart(trans, screen)
+		getDartCos(g.tick).applyTransformation(trans).draw(screen)
 	}
 
 	for trans := range g.kites.items {
-		drawKite(trans, screen)
+		getKiteCos(g.tick).applyTransformation(trans).draw(screen)
 	}
 }
 
-func drawDart(trans transformation, screen *ebiten.Image) {
+// We presume that the reciever is either a variant of a kite or a dart. This is important for the way the triangles are drawn.
+func (tile polygon) draw(screen *ebiten.Image) {
 	// coordinates to vertices
 	vertices := []ebiten.Vertex{}
 
-	for _, co := range dart.applyTransformation(trans).points {
+	for _, co := range tile.points {
 		vertices = append(vertices, newDartVertex(co))
 	}
 
-	// indices array
+	// indices array, since this is the same for kites and darts we just magic value hard code it.
 	indices := []uint16{0, 1, 2, 0, 2, 3}
 
 	screen.DrawTriangles(vertices, indices, whiteImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image), &ebiten.DrawTrianglesOptions{})
 }
 
-func drawKite(trans transformation, screen *ebiten.Image) {
-	// coordinates to vertices
-	vertices := []ebiten.Vertex{}
+func getDartCos(tick uint32) *polygon {
+	pointAngle := float64(tick) / float64(animationLength) * radian36
 
-	for _, co := range kite.applyTransformation(trans).points {
-		vertices = append(vertices, newKiteVertex(co))
+	return &polygon{
+		[]coordinate{
+			{0, 0},
+			{math.Cos(pointAngle), math.Sin(pointAngle)},
+			{1, 0},
+			{math.Cos(pointAngle), -math.Sin(pointAngle)},
+		},
 	}
+}
 
-	// indices array
-	indices := []uint16{0, 1, 2, 0, 2, 3}
+func getKiteCos(tick uint32) *polygon {
+	pointAngle := float64(tick) / float64(animationLength) * radian36
 
-	screen.DrawTriangles(vertices, indices, whiteImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image), &ebiten.DrawTrianglesOptions{})
+	return &polygon{
+		[]coordinate{
+			{0, 0},
+			{math.Cos(pointAngle), math.Sin(pointAngle)},
+			{1 / math.Phi, 0},
+			{math.Cos(pointAngle), -math.Sin(pointAngle)},
+		},
+	}
 }
 
 func newDartVertex(co coordinate) ebiten.Vertex {
--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main() {
 	ebiten.SetWindowTitle("Penrose")
 	dartTranses, kiteTranses := calculateDrawing()
 	game := Game{
-		dartTranses, kiteTranses,
+		transformation, kiteTranses, 0,
 	}
 	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
